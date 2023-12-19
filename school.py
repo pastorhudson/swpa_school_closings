3edf302045c1817b0febf619c46f761a4dde07d3
Ron Hudson <3606628+pastorhudson@users.noreply.github.com>
2023-12-19 11:26:46 -0500

    Add docstrings to functions in school.py
    Detailed explanations have been added for all functions within the 'school.py' file, in the form of docstrings. This includes the main function and subsidiary functions for reading and writing to the config file, adding and removing schools, and displaying school information.

--- school.py
+++ school.py
@@ -3,35 +3,84 @@ import xml.etree.ElementTree as ET
 import configparser
 import argparse
 
+
 def read_config(section, key):
+    """
+    Read a config value from a given section and key.
+
+    :param section: The section name in the config file.
+    :param key: The key name within the section.
+    :return: A list of lowercased, stripped strings from the config value.
+    """
     config = configparser.ConfigParser()
     config.read('config.ini')
     return [name.strip().lower() for name in config.get(section, key).split(',')]
 
+
 def write_config(section, key, school_names):
+    """
+    Write the given school names to the specified section and key in the configuration file.
+
+    :param section: The section in the configuration file.
+    :param key: The key within the section in the configuration file.
+    :param school_names: The list of school names to be written to the configuration file.
+    :return: None
+
+    Example usage:
+        write_config('General', 'Schools', ['School A', 'School B', 'School C'])
+    """
     config = configparser.ConfigParser()
     config.read('config.ini')
     config.set(section, key, ', '.join(school_names))
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
+
 def add_school(school_name):
+    """
+    Adds a school to the list of school names.
+
+    :param school_name: The name of the school to add.
+    :type school_name: str
+    :return: None
+    """
     school_names = read_config('SCHOOL_NAMES', 'names')
     school_names.append(school_name.lower())
     write_config('SCHOOL_NAMES', 'names', school_names)
 
+
 def remove_school(school_name):
+    """
+    Remove a school from the list of school names.
+
+    :param school_name: The name of the school to remove.
+    :type school_name: str
+    :return: None
+    :rtype: None
+    """
     school_names = read_config('SCHOOL_NAMES', 'names')
     school_names = [name for name in school_names if name != school_name.lower()]
     write_config('SCHOOL_NAMES', 'names', school_names)
 
+
 def show_schools():
+    """
+    Shows the current schools in the configuration.
+
+    :return: None
+    """
     school_names = read_config('SCHOOL_NAMES', 'names')
     print("Current schools in config:")
     for name in school_names:
         print(f" - {name}")
 
+
 def show_all_schools():
+    """
+    Retrieves and displays information about all schools from a given XML feed.
+
+    :return: None
+    """
     url = "https://assets1.cbsnewsstatic.com/Integrations/SchoolClosings/PRODUCTION/CBS/kdka/NEWSROOM/KDKAclosings.xml"
     with urllib.request.urlopen(url) as response:
         content = response.read()
@@ -42,7 +91,32 @@ def show_all_schools():
         status_name = record.find('FORCED_STATUS_NAME').text
         print(f"{org_name} - {status_name}")
 
+
 def main():
+    """
+    Main
+
+    This method manages school names in the config and fetches their status from a specified feed.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Example Usage:
+        To add a school name to the config:
+        main --add <school_name>
+
+        To remove a school name from the config:
+        main --remove <school_name>
+
+        To show current school names in the config:
+        main --show
+
+        To show all schools from the feed:
+        main --all
+    """
     parser = argparse.ArgumentParser(description='Manage school names in config and fetch status.')
     parser.add_argument('--add', help='Add a school name to config')
     parser.add_argument('--remove', help='Remove a school name from config')
